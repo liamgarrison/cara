1e5bdc0e816252ea79061e76dbfd8cbc8bf5a98e
Liam Garrison <liamgarrison@gmail.com>
2019-05-16 17:01:42 +0100

    added homepage and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  
-  resources :vehicles except: [:new, :create]
+  resources :vehicles, except: [:new, :create]
 
   resources :users, only: [] do
     resources :vehicles, only: [:new, :create]
   end
 
-  resources :bookings, only: [:new, :create, :show, :index] do
-    resources :vehicles, only: [:new, :create]
-  end
+  devise_for :users
+  root to: 'pages#home'
 end
