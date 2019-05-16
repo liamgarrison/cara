301a99734a0676eaae3f542384087c6475e6857d
Che To <chekinto@hotmail.com>
2019-05-16 16:10:45 +0100

    added nested resources for vehicles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :vehicles
+  resources :vehicles except: [:new, :create]
+
+  resources :users, only: [] do
+    resources :vehicles, only: [:new, :create]
+  end
+
   devise_for :users
   root to: 'pages#home'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 jim = User.create(first_name: "Jim", last_name: "White", email: "jimwhite@gmail.com", password: 1234)
-
 Vehicle.create(name: "Misty", berths: 2, owner: jim, address: "138 Kingsland Road, London", price_per_night: 22, category: "Caravan", description: "lorem " * 30)
