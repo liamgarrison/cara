b1bf9021b787b0841c4ac4a647576b452467bc1c
Che To <chekinto@hotmail.com>
2019-05-16 17:53:56 +0100

    added simple form and added some functionality to new and show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
-  resources :vehicles except: [:new, :create]
+
+  resources :vehicles, except: [:new, :create]
 
   resources :users, only: [] do
     resources :vehicles, only: [:new, :create]
