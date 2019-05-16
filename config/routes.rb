fe61da1bef5ca3a69db1f8f548ed37eab4af988a
Tom Dove <dovet@live.com>
2019-05-16 16:10:40 +0100

    Made amendments to route paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/new'
-  get 'bookings/create'
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bookings, only: [:new, :create, :show, :index] do
+    resources :vehicles, only: [:new, :create]
+  end
 end
