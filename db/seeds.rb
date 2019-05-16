5934cc546bf0f17cb99f70a54d0fc4336811b894
Liam Garrison <liamgarrison@gmail.com>
2019-05-16 16:17:44 +0100

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Booking.destroy_all
+Vehicle.destroy_all
+User.destroy_all
+
+# User seeds
+10.times do
+  User.create!({
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: 123456
+  })
+end
+
+10.times do
+  Vehicle.create!({
+    owner: User.all.sample,
+    name: Faker::Name.first_name,
+    address: Faker::Address.full_address,
+    berths: (1..5).to_a.sample,
+    description: Faker::Lorem.sentences(10),
+    price_per_night: (10..50).to_a.sample,
+    category: ["Campervan", "Caravan", "Trailer Tent", "Motorhome"].sample
+  })
+end
+
+10.times do
+  Booking.create!({
+    vehicle: Vehicle.all.sample,
+    renter: User.all.sample,
+    start_date: Date.new(2019, 6, 15),
+    number_of_nights: (1..5).to_a.sample
+  })
+end
