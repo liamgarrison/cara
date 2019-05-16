ff566ab0381afd70f8d9a9103e2e2e35356df4c3
Che To <chekinto@hotmail.com>
2019-05-16 15:47:41 +0100

    index page generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+jim = User.create(first_name: "Jim", last_name: "White", email: "jimwhite@gmail.com", password: 1234)
+
+Vehicle.create(name: "Misty", berths: 2, owner: jim, address: "138 Kingsland Road, London", price_per_night: 22, category: "Caravan", description: "lorem " * 30)
