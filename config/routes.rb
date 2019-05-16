ac2506ed6d98fa32039dbca1401c1577a4d5a573
Tom Dove <dovet@live.com>
2019-05-16 16:48:57 +0100

    Made amendments to routes, added HTML for bookings index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  
-  resources :vehicles except: [:new, :create]
+
+  resources :vehicles, except: [:new, :create]
 
   resources :users, only: [] do
     resources :vehicles, only: [:new, :create]
   end
 
-  resources :bookings, only: [:new, :create, :show, :index] do
-    resources :vehicles, only: [:new, :create]
+  resources :vehicles, only: [:new, :create] do
+    resources :bookings, only: [:new, :create]
   end
+
+  resources :bookings, only: [:show, :index]
 end
