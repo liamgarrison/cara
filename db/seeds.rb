db6871d925785ecd544244a86bddef6d3aa2a26f
Corrie Mosley <corrie.mosley@gmail.com>
2019-05-17 12:39:19 +0100

    Photo added to user model, seed user photos added, photos added to navbar and vehicle show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ User.destroy_all
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
     email: Faker::Internet.email,
-    password: 123456
+    password: 123456,
+    remote_photo_url: "https://kitt.lewagon.com/placeholder/users/liamgarrison"
   })
 end
 
